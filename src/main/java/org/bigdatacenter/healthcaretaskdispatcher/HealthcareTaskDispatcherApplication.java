19d100881d4d8359015f4a0eff11f3bed87876fe
Anthony-Kim <anthonykim1985@gmail.com>
2017-10-17 14:22:41 +0900

    Initial commit

---
+++ src/main/java/org/bigdatacenter/healthcaretaskdispatcher/HealthcareTaskDispatcherApplication.java
@@ -0,0 +1,12 @@
+package org.bigdatacenter.healthcaretaskdispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareTaskDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareTaskDispatcherApplication.class, args);
+	}
+}
